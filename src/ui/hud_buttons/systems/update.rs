2ffd4895e9bfe4181659b1f21c0042403335ac0f
Sven Richter <sveri80@gmail.com>
2023-08-02 23:38:57 +0200

    error if neither ship nor waypoint is selected

--- src/ui/hud_buttons/systems/update.rs
+++ src/ui/hud_buttons/systems/update.rs
@@ -4,14 +4,16 @@ use crate::{
     st_client,
     ui::{
         controls::components::{SelectedShip, SelectedWaypoint},
-        hud_buttons::components::*, hud::components::ErrorText,
+        hud::components::ErrorText,
+        hud_buttons::components::*,
     },
 };
 
 #[allow(clippy::type_complexity)]
 pub(crate) fn orbit_clicked(
     mut interaction_query: Query<(&Interaction, &Children), (Changed<Interaction>, With<OrbitButton>)>,
-    mut text_query: Query<&mut Text, Without<ErrorText>>, selected_ship: Query<&SelectedShip>, mut error_text: Query<&mut Text, With<ErrorText>>,
+    mut text_query: Query<&mut Text, Without<ErrorText>>, selected_ship: Query<&SelectedShip>,
+    mut error_text: Query<&mut Text, With<ErrorText>>,
 ) {
     for (interaction, children) in &mut interaction_query {
         let mut text = text_query.get_mut(children[0]).unwrap();
@@ -30,24 +32,19 @@ pub(crate) fn orbit_clicked(
 #[allow(clippy::type_complexity)]
 pub(crate) fn move_ship_clicked(
     mut move_query: Query<(&Interaction, &Children), (Changed<Interaction>, With<MoveButton>)>,
-    mut text_query: Query<&mut Text>,
-    selected_ship: Query<&SelectedShip>,
-    selected_waypoint: Query<&SelectedWaypoint>,
+    mut text_query: Query<&mut Text, Without<ErrorText>>, selected_ship: Query<&SelectedShip>,
+    selected_waypoint: Query<&SelectedWaypoint>, mut error_text: Query<&mut Text, With<ErrorText>>,
 ) {
     for (interaction, children) in &mut move_query {
         let mut text = text_query.get_mut(children[0]).unwrap();
         if *interaction == Interaction::Pressed {
-            text.sections[0].value = "Press".to_string();
-            if let Ok(waypoint) = selected_waypoint.get_single() {
-                dbg!("found");
+            if let (Ok(waypoint), Ok(ship)) = (selected_waypoint.get_single(), selected_ship.get_single()) {
+                text.sections[0].value = "Moving".to_string();
+                let res = st_client::move_ship(ship.ship.symbol.as_str(), waypoint.waypoint.symbol.to_string());
+                dbg!(res);
             } else {
-                dbg!("error");
+                error_text.single_mut().sections[0].value = "Error: You must select a ship and a waypoint".to_string();
             }
-            // let res = st_client::move_ship(
-            //     selected_ship.get_single().unwrap().ship.symbol.as_str(),
-            //     selected_waypoint.get_single().unwrap().waypoint.symbol.to_string(),
-            // );
-            // dbg!(res);
         }
     }
 }

87ef6554cf244b39c65c358ef9b0a53309ee90c8
Sven Richter <sveri80@gmail.com>
2023-08-02 23:29:53 +0200

    showing proper error when not selecting a ship

--- src/ui/hud_buttons/systems/update.rs
+++ src/ui/hud_buttons/systems/update.rs
@@ -4,26 +4,25 @@ use crate::{
     st_client,
     ui::{
         controls::components::{SelectedShip, SelectedWaypoint},
-        hud_buttons::components::*,
+        hud_buttons::components::*, hud::components::ErrorText,
     },
 };
 
 #[allow(clippy::type_complexity)]
 pub(crate) fn orbit_clicked(
     mut interaction_query: Query<(&Interaction, &Children), (Changed<Interaction>, With<OrbitButton>)>,
-    mut text_query: Query<&mut Text>, selected_ship: Query<&SelectedShip>,
+    mut text_query: Query<&mut Text, Without<ErrorText>>, selected_ship: Query<&SelectedShip>, mut error_text: Query<&mut Text, With<ErrorText>>,
 ) {
     for (interaction, children) in &mut interaction_query {
         let mut text = text_query.get_mut(children[0]).unwrap();
         if *interaction == Interaction::Pressed {
-            text.sections[0].value = "Orbiting".to_string();
             if let Ok(selected_ship) = selected_ship.get_single() {
-                dbg!("found");
+                let res = st_client::orbit_ship(selected_ship.ship.symbol.as_str());
+                text.sections[0].value = "Orbiting".to_string();
+                dbg!(res);
             } else {
-                dbg!("error");
+                error_text.single_mut().sections[0].value = "Error: No Ship Selected".to_string();
             }
-            // let res = st_client::orbit_ship(selected_ship.get_single().unwrap().ship.symbol.as_str());
-            // dbg!(res);
         }
     }
 }

d358759ab513d99424be09598b56a79dcb7cb5d5
Sven Richter <sveri80@gmail.com>
2023-08-04 19:53:11 +0200

    updating nav after moving not working

--- src/ui/hud_buttons/systems/update.rs
+++ src/ui/hud_buttons/systems/update.rs
@@ -1,12 +1,12 @@
 use bevy::{ecs::query::WorldQuery, prelude::*};
 
 use crate::{
-    st_client,
+    st_client::{self, GenericResponse},
     ui::{
-        controls::components::{SelectedShip, SelectedWaypoint},
+        controls::{components::{SelectedShip, SelectedWaypoint}, self},
         hud::components::ErrorText,
         hud_buttons::components::*,
-    },
+    }, game::ship::components::{Nav, Ship},
 };
 
 #[allow(clippy::type_complexity)]
@@ -32,15 +32,26 @@ pub(crate) fn orbit_clicked(
 #[allow(clippy::type_complexity)]
 pub(crate) fn move_ship_clicked(
     mut move_query: Query<(&Interaction, &Children), (Changed<Interaction>, With<MoveButton>)>,
-    mut text_query: Query<&mut Text, Without<ErrorText>>, selected_ship: Query<&SelectedShip>,
+    mut text_query: Query<&mut Text, Without<ErrorText>>, mut selected_ship: Query<&SelectedShip>,
     selected_waypoint: Query<&SelectedWaypoint>, mut error_text: Query<&mut Text, With<ErrorText>>,
+    mut ships: Query<&mut Ship>
 ) {
     for (interaction, children) in &mut move_query {
         let mut text = text_query.get_mut(children[0]).unwrap();
         if *interaction == Interaction::Pressed {
-            if let (Ok(waypoint), Ok(ship)) = (selected_waypoint.get_single(), selected_ship.get_single()) {
-                text.sections[0].value = "Moving".to_string();
-                let res = st_client::move_ship(ship.ship.symbol.as_str(), waypoint.waypoint.symbol.to_string());
+            // Query::get_mut(&mut self, entity);
+            // let mut ship: &mut Ship = selected_ship.get_single_mut().unwrap().ship;
+            if let (Ok(waypoint), Ok(ship)) = (selected_waypoint.get_single(), selected_ship.get_single_mut()) {
+                // if let (Ok(waypoint), ) = (selected_waypoint.get_single(), ) {
+                    text.sections[0].value = "Moving".to_string();
+                    let res = st_client::move_ship(ship.ship.symbol.as_str(), waypoint.waypoint.symbol.to_string());
+                    let nav_details: GenericResponse<Nav> = serde_json::from_str(&res).unwrap();
+                    for mut ship_entity in &mut ships {
+                        if ship_entity.symbol  == ship.ship.symbol {
+                            ship_entity.nav = nav_details.data.clone();
+                        }
+                    }
+                // ship.ship.nav = nav_details.data;
                 dbg!(res);
             } else {
                 error_text.single_mut().sections[0].value = "Error: You must select a ship and a waypoint".to_string();

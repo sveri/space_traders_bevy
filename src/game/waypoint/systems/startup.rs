512c2279ff9cd47eb296a9e0029f99e8c1b37b2d
Sven Richter <sveri80@gmail.com>
2023-08-05 11:05:10 +0200

    added planet sprites

--- src/game/waypoint/systems/startup.rs
+++ src/game/waypoint/systems/startup.rs
@@ -1,19 +1,26 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_mod_picking::{PickableBundle, prelude::{RaycastPickTarget, Pointer, On, Click}};
 
-use crate::st_client;
+use crate::{st_client, game::waypoint::components::Waypoint};
 
-pub(crate) fn add_waypoints(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+pub(crate) fn add_waypoints(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>, asset_server: Res<AssetServer>) {
     let agent_details = st_client::fetch_agent_details();
     let waypoints = st_client::fetch_waypoints(agent_details.get_headquarter_system_symbol().as_str());
     waypoints.iter().for_each(|waypoint| {
+        let mut planet_transform = Transform::from_xyz(waypoint.x, waypoint.y, 0.0);
+        planet_transform.scale = Vec3::new(0.01, 0.01, 0.01);
         commands.spawn((
             waypoint.to_owned(),
-            MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(waypoint.get_display_size()).into()).into(),
-                material: materials.add(ColorMaterial::from(Color::PURPLE)),
-                transform: Transform::from_translation(Vec3::new(waypoint.x, waypoint.y, 0.)),
+            SpriteBundle {
+                transform: planet_transform,
+                texture: asset_server.load("planets/planet02.png"),
                 ..default()
             },
+            PickableBundle::default(),
+            RaycastPickTarget::default(),
+            On::<Pointer<Click>>::target_component_mut::<Waypoint>(|click, waypoint_rep| {
+                println!("{:?}", waypoint_rep);
+            }),
         ));
     })
 }

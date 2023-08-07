84edf96a8536eb65cf4ccf8cd904342b18ac189f
Sven Richter <sveri80@gmail.com>
2023-08-07 21:25:55 +0200

    cleanup

--- src/ui/hud_buttons/systems/events.rs
+++ src/ui/hud_buttons/systems/events.rs
@@ -31,12 +31,9 @@ struct NavWrapper {
 pub(crate) fn handle_orbit_clicked_event(
     selected_ship: Query<&SelectedShip>, mut error_text: Query<&mut Text, With<ErrorText>>, mut ships: Query<&mut Ship>,
 ) {
-    // dbg!("orbit clicked");
     dbg!(&selected_ship.iter().len());
     if let Ok(selected_ship) = selected_ship.get_single() {
-        dbg!(&selected_ship);
         let res = st_client::orbit_ship(selected_ship.ship.symbol.as_str());
-        // text.sections[0].value = "Orbiting".to_string();
         match serde_json::from_str::<GenericResponse<NavWrapper>>(&res) {
             Ok(nav_details) => {
                 for mut ship_entity in &mut ships {
@@ -50,24 +47,7 @@ pub(crate) fn handle_orbit_clicked_event(
                 panic!("Error reading navigation data when orbiting ship: {e}");
             }
         }
-        // dbg!(&res);
-        // let nav_details: GenericResponse<Nav> = serde_json::from_str(&res).unwrap();
-        // dbg!("selected");
     } else {
-        // let mut text = text_query.get_mut(children[0]).unwrap();
-        // dbg!("none selected");
         error_text.single_mut().sections[0].value = "Error: No Ship Selected".to_string();
     }
 }
-// pub(crate) fn handle_orbit_clicked_event(
-//     mut event: EventReader<OrbitClicked>,
-//     // mut clicked_button: Query<&Button>,
-// ) {
-//     for select_event in event.iter() {
-//         dbg!(&event.iter().len());
-//         dbg!("clicked_button: {:?}", &select_event);
-//         // if let Ok((_entity, waypoint)) = clicked_button.get(select_event.0) {
-//         //     // clicked_button.single_mut().sections[0].value = format!("Selected Waypoint: {}, Traits: {}", waypoint.symbol, waypoint.get_traits());
-//         // }
-//     }
-// }

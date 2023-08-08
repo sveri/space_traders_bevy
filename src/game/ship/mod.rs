c5a8998b981e1343a9038c55f1bc36598b69c257
Sven Richter <sveri80@gmail.com>
2023-08-08 13:43:53 +0200

    more cleanup

--- src/game/ship/mod.rs
+++ src/game/ship/mod.rs
@@ -6,16 +6,19 @@ use std::time::Duration;
 
 use bevy::{prelude::*, time::common_conditions::on_timer};
 
-use self::systems::{update::update_ships, startup::add_ships, events::{ShipSelected, handle_ship_selected_event}};
-
+use self::systems::{
+    events::{handle_ship_selected_event, ShipSelected},
+    startup::add_ships,
+    update::update_ships,
+};
 
 pub(crate) struct ShipPlugin;
 
 impl Plugin for ShipPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_ships)
+            .add_event::<ShipSelected>()
             .add_systems(Update, update_ships.run_if(on_timer(Duration::from_secs_f64(1.0))))
-            .add_systems(Update, handle_ship_selected_event)
-            .add_event::<ShipSelected>();
+            .add_systems(Update, handle_ship_selected_event.run_if(on_event::<ShipSelected>()));
     }
 }
--- src/game/waypoint/mod.rs
+++ src/game/waypoint/mod.rs
@@ -3,14 +3,17 @@ pub(super) mod systems;
 
 use bevy::prelude::*;
 
-use self::systems::{startup::add_waypoints, events::{handle_waypoint_selected_event, WaypointSelected}};
+use self::systems::{
+    events::{handle_waypoint_selected_event, WaypointSelected},
+    startup::add_waypoints,
+};
 
 pub(crate) struct WaypointPlugin;
 
 impl Plugin for WaypointPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_waypoints)
-        .add_systems(Update, handle_waypoint_selected_event)
-        .add_event::<WaypointSelected>();
+            .add_event::<WaypointSelected>()
+            .add_systems(Update, handle_waypoint_selected_event.run_if(on_event::<WaypointSelected>()));
     }
 }
